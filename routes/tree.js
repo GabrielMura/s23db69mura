15599deff7072ed1ff0095a57e3a6f20f1813f42
GabrielMura <78869418+GabrielMura@users.noreply.github.com>
2023-04-27 17:07:25 -0500

    finished assignment

--- routes/tree.js
+++ routes/tree.js
@@ -1,6 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
+// A little function to check if we have an authorized user and continue on or
+// redirect to login.
+const secured = (req, res, next) => {
+    if (req.user){
+    return next();
+   }
+   req.session.returnTo = req.originalUrl;
+   res.redirect("/login");
+   }
+
 const tree_controlers= require('../controllers/tree');
 var router = express.Router();
 /* GET costumes */
@@ -10,6 +20,8 @@ router.get('/detail', tree_controlers.tree_view_one_Page);
 /* GET create costume page */
 router.get('/create', tree_controlers.tree_create_Page);
 /* GET create update page */
-router.get('/update', tree_controlers.tree_update_Page);
+router.get('/update', secured, tree_controlers.tree_update_Page);
+/* GET delete costume page */
+router.get('/delete', tree_controlers.tree_delete_Page);
 
 module.exports = router;
